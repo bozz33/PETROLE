311eb836434c726a9beb297c9e5c7ff2e09c0ec8
bozz33 <48349707+bozz33@users.noreply.github.com>
2026-08-10 08:02:09 +0000

    feat(leak): exposer le bilan matière compensé

--- packages/leak_detection/hydro_leak/__init__.py
+++ packages/leak_detection/hydro_leak/__init__.py
@@ -1,5 +1,16 @@
 """Fondations d'évaluation de la détection de fuite PETROLE."""
 
+from hydro_leak.material_balance import (
+    MaterialBalanceResult,
+    MaterialBalanceWindow,
+    compute_material_balance,
+)
 from hydro_leak.metrics import DetectionPerformance, detection_performance
 
-__all__ = ["DetectionPerformance", "detection_performance"]
+__all__ = [
+    "DetectionPerformance",
+    "MaterialBalanceResult",
+    "MaterialBalanceWindow",
+    "compute_material_balance",
+    "detection_performance",
+]

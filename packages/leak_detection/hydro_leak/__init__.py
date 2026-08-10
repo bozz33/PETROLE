3178dc7807109748d5ea2cd8a457c1d52f989546
bozz33 <48349707+bozz33@users.noreply.github.com>
2026-08-10 06:26:51 +0000

    feat(leak): créer le domaine d'évaluation LDS

---
+++ packages/leak_detection/hydro_leak/__init__.py
@@ -0,0 +1,5 @@
+"""Fondations d'évaluation de la détection de fuite PETROLE."""
+
+from hydro_leak.metrics import DetectionPerformance, detection_performance
+
+__all__ = ["DetectionPerformance", "detection_performance"]
